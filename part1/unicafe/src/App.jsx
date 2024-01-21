24aa96135aacb659fa2ab25a12c656ed6cf2ada2
Tony Ramírez <tonyramirez.business@outlook.com>
2024-01-21 14:19:58 -0600

    feat(part1/unicafe): add component for displaying statistics
    exercise 1.8

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -8,31 +8,39 @@ const Statistic = ({ text, value }) => (
   </p>
 );
 
+const Statistics = ({ good, neutral, bad }) => {
+  const all = good + neutral + bad;
+  const average = (good - bad) / all || 0;
+  const positive = (good / all) * 100 || 0;
+
+  return (
+    <>
+      <h1>statistics</h1>
+      <Statistic text="good" value={good} />
+      <Statistic text="neutral" value={neutral} />
+      <Statistic text="bad" value={bad} />
+      <Statistic text="average" value={average} />
+      <Statistic text="positive" value={`${positive} %`} />
+    </>
+  );
+};
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
-  const [average, setAverage] = useState(0);
-  const [positive, setPositive] = useState(0);
-
   const handleGoodClick = () => {
     setGood(good + 1);
-    setAverage((good + 1 - bad) / (good + 1 + neutral + bad));
-    setPositive(((good + 1) / (good + 1 + neutral + bad)) * 100);
   };
 
   const handleNeutralClick = () => {
     setNeutral(neutral + 1);
-    setAverage((good - bad) / (good + neutral + 1 + bad));
-    setPositive((good / (good + neutral + 1 + bad)) * 100);
   };
 
   const handleBadClick = () => {
     setBad(bad + 1);
-    setAverage((good - (bad + 1)) / (good + neutral + bad + 1));
-    setPositive((good / (good + neutral + bad + 1)) * 100);
   };
 
   return (
@@ -41,12 +49,7 @@ const App = () => {
       <Button onClick={handleGoodClick} text="good" />
       <Button onClick={handleNeutralClick} text="neutral" />
       <Button onClick={handleBadClick} text="bad" />
-      <h1>statistics</h1>
-      <Statistic text="good" value={good} />
-      <Statistic text="neutral" value={neutral} />
-      <Statistic text="bad" value={bad} />
-      <Statistic text="average" value={average} />
-      <Statistic text="positive" value={`${positive} %`} />
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </>
   );
 };
