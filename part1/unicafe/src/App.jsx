58080c0109b5e8304d96e7c6fb17c5695e5151ad
Tony Ramírez <tonyramirez.business@outlook.com>
2024-01-21 14:14:48 -0600

    feat(part1/unicafe): add average and percentage of positive statistics
    exercise 1.7

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -14,16 +14,39 @@ const App = () => {
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
+  const [average, setAverage] = useState(0);
+  const [positive, setPositive] = useState(0);
+
+  const handleGoodClick = () => {
+    setGood(good + 1);
+    setAverage((good + 1 - bad) / (good + 1 + neutral + bad));
+    setPositive(((good + 1) / (good + 1 + neutral + bad)) * 100);
+  };
+
+  const handleNeutralClick = () => {
+    setNeutral(neutral + 1);
+    setAverage((good - bad) / (good + neutral + 1 + bad));
+    setPositive((good / (good + neutral + 1 + bad)) * 100);
+  };
+
+  const handleBadClick = () => {
+    setBad(bad + 1);
+    setAverage((good - (bad + 1)) / (good + neutral + bad + 1));
+    setPositive((good / (good + neutral + bad + 1)) * 100);
+  };
+
   return (
     <>
       <h1>give feedback</h1>
-      <Button onClick={() => setGood(good + 1)} text="good" />
-      <Button onClick={() => setNeutral(neutral + 1)} text="neutral" />
-      <Button onClick={() => setBad(bad + 1)} text="bad" />
+      <Button onClick={handleGoodClick} text="good" />
+      <Button onClick={handleNeutralClick} text="neutral" />
+      <Button onClick={handleBadClick} text="bad" />
       <h1>statistics</h1>
       <Statistic text="good" value={good} />
       <Statistic text="neutral" value={neutral} />
       <Statistic text="bad" value={bad} />
+      <Statistic text="average" value={average} />
+      <Statistic text="positive" value={`${positive} %`} />
     </>
   );
 };
