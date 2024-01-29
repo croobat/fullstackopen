57ae93d8afb3dd6d80314408d9234614c94efbb9
Tony Ramírez <tonyramirez.business@outlook.com>
2024-01-29 12:35:41 -0600

    feat(part3/coursedata): add eslint

--- part3/coursedata-back/index.js
+++ part3/coursedata-back/index.js
@@ -1,101 +1,101 @@
-require('dotenv').config();
+require('dotenv').config()
 
-const express = require('express');
-const cors = require('cors');
-const app = express();
+const express = require('express')
+const cors = require('cors')
+const app = express()
 
-const Note = require('./models/note');
+const Note = require('./models/note')
 
 const requestLogger = (req, _, next) => {
-  console.log('Method:', req.method);
-  console.log('Path:  ', req.path);
-  console.log('Body:  ', req.body);
-  console.log('---');
-  next();
-};
+  console.log('Method:', req.method)
+  console.log('Path:  ', req.path)
+  console.log('Body:  ', req.body)
+  console.log('---')
+  next()
+}
 
 const unknownEndpoint = (_, res) => {
-  res.status(404).send({ error: 'unknown endpoint' });
-};
+  res.status(404).send({ error: 'unknown endpoint' })
+}
 
 const errorHandler = (error, _, res, next) => {
-  console.error(error.message);
+  console.error(error.message)
 
   if (error.name === 'CastError') {
-    return res.status(400).send({ error: 'malformatted id' });
+    return res.status(400).send({ error: 'malformatted id' })
   } else if (error.name === 'ValidationError') {
-    return res.status(400).json({ error: error.message });
+    return res.status(400).json({ error: error.message })
   }
 
-  next(error);
-};
+  next(error)
+}
 
-app.use(express.static('dist'));
-app.use(express.json());
-app.use(requestLogger);
-app.use(cors());
+app.use(express.static('dist'))
+app.use(express.json())
+app.use(requestLogger)
+app.use(cors())
 // error handler middleware should be the last loaded middleware
-app.use(errorHandler);
+app.use(errorHandler)
 
 app.get('/', (_, res) => {
-  res.send('<h1>Hello World!</h1>');
-});
+  res.send('<h1>Hello World!</h1>')
+})
 
 app.get('/api/notes', (_, res) => {
   Note.find({}).then((notes) => {
-    res.json(notes);
-  });
-});
+    res.json(notes)
+  })
+})
 
 app.get('/api/notes/:id', (req, res, next) => {
   Note.findById(req.params.id)
     .then((note) => {
       if (note) {
-        res.json(note);
+        res.json(note)
       } else {
-        res.status(404).end();
+        res.status(404).end()
       }
     })
-    .catch((error) => next(error));
-});
+    .catch((error) => next(error))
+})
 
 app.post('/api/notes', (req, res, next) => {
-  const body = req.body;
+  const body = req.body
 
   const note = new Note({
     content: body.content,
     important: body.important || false,
-  });
+  })
 
   note
     .save()
     .then((savedNote) => {
-      res.json(savedNote);
+      res.json(savedNote)
     })
-    .catch((error) => next(error));
-});
+    .catch((error) => next(error))
+})
 
 app.put('/api/notes/:id', (req, res, next) => {
-  const { content, important } = req.body;
+  const { content, important } = req.body
 
   Note.findByIdAndUpdate(req.params.id, { content, important }, { new: true, runValidators: true, context: 'query' })
     .then((updatedNote) => {
-      res.json(updatedNote);
+      res.json(updatedNote)
     })
-    .catch((error) => next(error));
-});
+    .catch((error) => next(error))
+})
 
 app.delete('/api/notes/:id', (req, res, next) => {
   Note.findByIdAndDelete(req.params.id)
     .then(() => {
-      res.status(204).end();
+      res.status(204).end()
     })
-    .catch((error) => next(error));
-});
+    .catch((error) => next(error))
+})
 
-app.use(unknownEndpoint);
+app.use(unknownEndpoint)
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+  console.log(`Server running on port ${PORT}`)
+})
--- part3/coursedata-back/mongo.js
+++ part3/coursedata-back/mongo.js
@@ -1,39 +1,28 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
 
 if (process.argv.length < 3) {
-  console.log('give password as argument');
-  process.exit(1);
+  console.log('give password as argument')
+  process.exit(1)
 }
 
-const password = process.argv[2];
+const password = process.argv[2]
 
-const url = `mongodb+srv://tony:${password}@cluster0.ajenvwu.mongodb.net/noteApp?retryWrites=true&w=majority`;
+const url = `mongodb+srv://tony:${password}@cluster0.ajenvwu.mongodb.net/noteApp?retryWrites=true&w=majority`
 
-mongoose.set('strictQuery', false);
-mongoose.connect(url);
+mongoose.set('strictQuery', false)
+mongoose.connect(url)
 
 const noteSchema = new mongoose.Schema({
   content: String,
   important: Boolean,
   date: Date,
-});
+})
 
-const Note = mongoose.model('Note', noteSchema);
-
-const note = new Note({
-  content: 'Hola soy Tony',
-  important: true,
-  date: new Date(),
-});
-
-// note.save().then((result) => {
-//   console.log('note saved!');
-//   mongoose.connection.close();
-// });
+const Note = mongoose.model('Note', noteSchema)
 
 Note.find({ important: true }).then((result) => {
   result.forEach((note) => {
-    console.log(note);
-  });
-  mongoose.connection.close();
-});
+    console.log(note)
+  })
+  mongoose.connection.close()
+})
