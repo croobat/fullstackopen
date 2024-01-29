d94896aae61b692c13658b56b015caacd2ba413d
Tony Ramírez <tonyramirez.business@outlook.com>
2024-01-29 11:42:12 -0600

    feat(part3/coursedata): add validation to note model

--- part3/coursedata-back/index.js
+++ part3/coursedata-back/index.js
@@ -23,6 +23,8 @@ const errorHandler = (error, _, res, next) => {
 
   if (error.name === 'CastError') {
     return res.status(400).send({ error: 'malformatted id' });
+  } else if (error.name === 'ValidationError') {
+    return res.status(400).json({ error: error.message });
   }
 
   next(error);
@@ -57,34 +59,26 @@ app.get('/api/notes/:id', (req, res, next) => {
     .catch((error) => next(error));
 });
 
-app.post('/api/notes', (req, res) => {
+app.post('/api/notes', (req, res, next) => {
   const body = req.body;
-
-  if (!body.content) {
-    return res.status(400).json({
-      error: 'content missing',
-    });
-  }
 
   const note = new Note({
     content: body.content,
     important: body.important || false,
   });
 
-  note.save().then((savedNote) => {
-    res.json(savedNote);
-  });
+  note
+    .save()
+    .then((savedNote) => {
+      res.json(savedNote);
+    })
+    .catch((error) => next(error));
 });
 
 app.put('/api/notes/:id', (req, res, next) => {
-  const body = req.body;
+  const { content, important } = req.body;
 
-  const note = {
-    content: body.content,
-    important: body.important,
-  };
-
-  Note.findByIdAndUpdate(req.params.id, note, { new: true })
+  Note.findByIdAndUpdate(req.params.id, { content, important }, { new: true, runValidators: true, context: 'query' })
     .then((updatedNote) => {
       res.json(updatedNote);
     })
--- part3/coursedata-back/models/note.js
+++ part3/coursedata-back/models/note.js
@@ -16,7 +16,11 @@ mongoose
   });
 
 const noteSchema = new mongoose.Schema({
-  content: String,
+  content: {
+    type: String,
+    minlength: 3,
+    required: true,
+  },
   important: Boolean,
 });
 
