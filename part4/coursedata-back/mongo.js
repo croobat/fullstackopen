73165cc41230a70f48e3713d32ca116795a71372
Tony Ramírez <tonyramirez.business@outlook.com>
2024-02-12 13:47:27 -0600

    feat(part4/coursedata): move part3 project

---
+++ part4/coursedata-back/mongo.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length < 3) {
+  console.log('give password as argument')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+
+const url = `mongodb+srv://tony:${password}@cluster0.ajenvwu.mongodb.net/noteApp?retryWrites=true&w=majority`
+
+mongoose.set('strictQuery', false)
+mongoose.connect(url)
+
+const noteSchema = new mongoose.Schema({
+  content: String,
+  important: Boolean,
+  date: Date,
+})
+
+const Note = mongoose.model('Note', noteSchema)
+
+Note.find({ important: true }).then((result) => {
+  result.forEach((note) => {
+    console.log(note)
+  })
+  mongoose.connection.close()
+})
