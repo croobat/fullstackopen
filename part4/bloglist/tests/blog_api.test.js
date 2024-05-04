22104207e2a7e4f762eba0355ac006666d2bd8f6
Tony Ramírez <tonyramirez.business@outlook.com>
2024-05-04 01:03:46 -0600

    feat(part4/bloglist): add delete blog functionality
    exercise 4.13

--- part4/bloglist/tests/blog_api.test.js
+++ part4/bloglist/tests/blog_api.test.js
@@ -19,62 +19,81 @@ beforeEach(async () => {
   await blogObject.save()
 })
 
-describe('blogs api', () => {
-  test('blogs are returned as json', async () => {
-    await api
-      .get('/api/blogs')
-      .expect(200)
-      .expect('Content-Type', /application\/json/)
+describe('when there is initially some blogs stored', () => {
+  describe('viewing', () => {
+    test('blogs are returned as json', async () => {
+      await api
+        .get('/api/blogs')
+        .expect(200)
+        .expect('Content-Type', /application\/json/)
+    })
+
+    test('blogs are returned with correct amount', async () => {
+      const response = await api.get('/api/blogs')
+      assert.equal(response.body.length, helper.initialBlogs.length)
+    })
+
+    test('blog id is named id instead of _id', async () => {
+      const response = await api.get('/api/blogs')
+      assert.ok(response.body[0].id)
+    })
+
+    test('if likes is missing, it is set to 0', async () => {
+      const newBlog = {
+        title: 'new blog',
+        url: 'http://newblog.com',
+      }
+      await api
+        .post('/api/blogs')
+        .send(newBlog)
+        .expect(201)
+        .expect('Content-Type', /application\/json/)
+
+      const response = await api.get('/api/blogs')
+      assert.equal(response.body[2].likes, 0)
+    })
   })
 
-  test('blogs are returned with correct amount', async () => {
-    const response = await api.get('/api/blogs')
-    assert.equal(response.body.length, helper.initialBlogs.length)
+  describe('adding', () => {
+    test('a valid blog can be added', async () => {
+      const newBlog = {
+        title: 'new blog',
+        url: 'http://newblog.com',
+      }
+      await api
+        .post('/api/blogs')
+        .send(newBlog)
+        .expect(201)
+        .expect('Content-Type', /application\/json/)
+
+      const response = await api.get('/api/blogs')
+      const titles = response.body.map((r) => r.title)
+
+      assert.ok(titles.includes('new blog'))
+    })
+
+    test('if title or url is missing, return 400', async () => {
+      const newBlog = {}
+      await api
+        .post('/api/blogs')
+        .send(newBlog)
+        .expect(400)
+    })
   })
 
-  test('blog id is named id instead of _id', async () => {
-    const response = await api.get('/api/blogs')
-    assert.ok(response.body[0].id)
-  })
+  describe('deleting', () => {
+    test('a blog can be deleted', async () => {
+      const response = await api.get('/api/blogs')
+      const { id } = response.body[0]
+      console.log('response.body)', response.body)
 
-  test('a valid blog can be added', async () => {
-    const newBlog = {
-      title: 'new blog',
-      url: 'http://newblog.com',
-    }
-    await api
-      .post('/api/blogs')
-      .send(newBlog)
-      .expect(201)
-      .expect('Content-Type', /application\/json/)
+      await api
+        .delete(`/api/blogs/${id}`)
+        .expect(204)
 
-    const response = await api.get('/api/blogs')
-    const titles = response.body.map((r) => r.title)
-
-    assert.ok(titles.includes('new blog'))
-  })
-
-  test('if likes is missing, it is set to 0', async () => {
-    const newBlog = {
-      title: 'new blog',
-      url: 'http://newblog.com',
-    }
-    await api
-      .post('/api/blogs')
-      .send(newBlog)
-      .expect(201)
-      .expect('Content-Type', /application\/json/)
-
-    const response = await api.get('/api/blogs')
-    assert.equal(response.body[2].likes, 0)
-  })
-
-  test('if title or url is missing, return 400', async () => {
-    const newBlog = {}
-    await api
-      .post('/api/blogs')
-      .send(newBlog)
-      .expect(400)
+      const blogsAtEnd = await helper.blogsInDb()
+      assert.equal(blogsAtEnd.length, helper.initialBlogs.length - 1)
+    })
   })
 
   after(async () => {
