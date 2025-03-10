d3ef68218a7417c915532b940b0391274480be92
Tony Ramírez <tonyramirez.business@outlook.com>
2025-03-09 22:30:41 -0600

    feat(part5/coursedata-front): add login functionality

--- part5/coursedata-front/src/App.jsx
+++ part5/coursedata-front/src/App.jsx
@@ -1,5 +1,8 @@
 import { useState, useEffect } from "react";
+
 import noteService from "./services/notes";
+import loginService from "./services/login";
+
 import Note from "./components/Note";
 import Notification from "./components/Notification";
 import Footer from "./components/Footer";
@@ -12,6 +15,7 @@ const App = () => {
   const [errorMessage, setErrorMessage] = useState("");
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const [user, setUser] = useState(null);
 
   const notesToShow = showAll ? notes : notes.filter((note) => note.important);
 
@@ -54,8 +58,53 @@ const App = () => {
 
   const handleLogin = async (event) => {
     event.preventDefault();
-    console.log("logging in with", username, password);
+
+    try {
+      const user = await loginService.login({
+        username,
+        password,
+      });
+      setUser(user);
+      setUsername("");
+      setPassword("");
+    } catch (exception) {
+      setErrorMessage("Wrong credentials");
+      setTimeout(() => {
+        setErrorMessage(null);
+      }, 5000);
+    }
   };
+
+  const loginForm = () => (
+    <form onSubmit={handleLogin}>
+      <div>
+        username
+        <input
+          type="text"
+          value={username}
+          name="Username"
+          onChange={({ target }) => setUsername(target.value)}
+        />
+      </div>
+      <div>
+        password
+        <input
+          type="password"
+          value={password}
+          name="Password"
+          onChange={({ target }) => setPassword(target.value)}
+        />
+      </div>
+      <button type="submit">login</button>
+    </form>
+  );
+
+  const noteForm = () => (
+    <form onSubmit={addNote}>
+      <input value={newNote} onChange={handleNoteChange} />
+      <button type="submit">save</button>
+    </form>
+  );
 
   useEffect(() => {
     noteService.getAll().then((initialNotes) => {
@@ -69,29 +118,14 @@ const App = () => {
 
       {errorMessage && <Notification message={errorMessage} />}
 
-      <form onSubmit={handleLogin}>
+      {user === null ? (
+        loginForm()
+      ) : (
         <div>
-          username
-          <input
-            type="text"
-            value={username}
-            name="Username"
-            onChange={({ target }) => setUsername(target.value)}
-          />
+          <p>{user.name} logged-in</p>
+          {noteForm()}
         </div>
-
-        <div>
-          password
-          <input
-            type="password"
-            value={password}
-            name="Password"
-            onChange={({ target }) => setPassword(target.value)}
-          />
-        </div>
-
-        <button type="submit">login</button>
-      </form>
+      )}
 
       <div>
         <button onClick={() => setShowAll(!showAll)}>
