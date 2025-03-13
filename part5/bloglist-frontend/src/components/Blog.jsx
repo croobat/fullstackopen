31a22ee7085dd9a9b6913995f1569ef48016cbc2
Tony Ramírez <tonyramirez.business@outlook.com>
2025-03-12 23:42:51 -0600

    feat(part5/bloglist-frontend): add Blog details view
    exercise 5.7

--- part5/bloglist-frontend/src/components/Blog.jsx
+++ part5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,46 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react";
 
-export default Blog
+const Blog = ({ blog }) => {
+  const [expanded, setExpanded] = useState(false);
+
+  return (
+    <div style={styles.blogStyle}>
+      <div style={styles.row}>
+        <div>
+          {blog.title} - {blog.author}
+        </div>
+
+        <button onClick={() => setExpanded(!expanded)}>
+          {expanded ? "hide" : "view"}
+        </button>
+      </div>
+
+      {expanded && (
+        <div>
+          <div>{blog.url}</div>
+          <div>
+            likes: {blog.likes}{" "}
+            <button onClick={() => console.log("like")}>like</button>
+          </div>
+          <div>{blog.user.name}</div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+const styles = {
+  blogStyle: {
+    padding: 8,
+    border: "solid",
+    borderWidth: 1,
+    marginBottom: 10,
+  },
+  row: {
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+};
+
+export default Blog;
